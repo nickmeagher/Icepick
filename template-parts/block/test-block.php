62c52e3b44a86b21302d04a9cae1458043fc7bfc
Grant Meagher <grantme@gher.com>
2019-10-11 14:45:01 -0500

    Default section labeled with classes
    Moving away from labeling sections with IDs by default. Will use classes from now on when viable.

--- template-parts/block/test-block.php
+++ template-parts/block/test-block.php
@@ -1,6 +1,6 @@
 <?php /* Block Name: Test Block */ ?>
 
-<section id="test-block">
+<section class="test-block">
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-6 mx-auto text-center">
