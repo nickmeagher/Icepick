da7b9bcbd3951c8891ada0c20168bf57eb57622e
Grant Meagher <grantme@gher.com>
2021-01-13 12:35:24 -0600

    Favicon / Gutenberg Width
    Removed custom theme favicon code in header.php - allowing for native WP favicon adustment.
    
    Also added CSS code that allows for Gutenberg editor to be full-width. This is better UI for devs & clients.

--- header.php
+++ header.php
@@ -18,20 +18,13 @@
 		<meta name="MobileOptimized" content="320">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-		<!-- icons & favicons -->
-		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-		<!--[if IE]>
-			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-		<![endif]-->
-
-  	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-    
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+	  	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+	    
+	    <!-- Bootstrap CSS -->
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+	    
+	    <!-- Font Awesome -->
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
     
 		<!-- wordpress head functions -->
 		<?php wp_head(); ?>
@@ -45,20 +38,21 @@
 	<body <?php body_class(); ?>>
   	
   	<nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container">
-          
-          <a class="navbar-brand" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
+  		<div class="container">
+        	
+        	<a class="navbar-brand" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
+        	
         	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content" aria-controls="navbar-content" aria-expanded="false" aria-label="<?php esc_html_e( 'Toggle Navigation', 'theme-textdomain' ); ?>">
         		<span class="navbar-toggler-icon"></span>
         	</button>
         
         	<div class="collapse navbar-collapse" id="navbar-content">
           	
-          	<?php primer_main_nav(); ?>
+			<?php primer_main_nav(); ?>
           	
         	</div><!-- end #navbar-content -->
             	
         </div><!-- end .container -->
-      </nav>
+    </nav>
 
     
