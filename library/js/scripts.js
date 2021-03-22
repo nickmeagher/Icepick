2f2ed5a3f241c991094db29799d1ac065944a4ea
Grant Meagher <grantme@gher.com>
2021-03-22 11:03:00 -0500

    Enable Mobile on New Bootstrap
    Adding mobile code/styling to latest nav update.

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -3,4 +3,16 @@ jQuery(document).ready(function($) {
 	// Initialize AOS
 	AOS.init();
 	
+	// Mobile Menu
+	$('#menu-nav-menu > .dropdown').append("<div class='nav-toggle'><i class='fas fa-chevron-down'></i></div>");
+	
+	$('#menu-nav-menu .dropdown .nav-toggle').click(function() {
+		$(this).siblings('ul.dropdown-menu').slideToggle(300);
+		$(this).toggleClass('active');
+	});
+	
+	$('.navbar-toggler').click(function() {
+		$('body').toggleClass('nav-open');
+	});
+	
 }); /* end of as page load scripts */
