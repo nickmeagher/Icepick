fd34f3fc9cc00505ef394d09558ae4b07b6a0d9d
Grant Meagher <grantme@gher.com>
2023-02-15 13:50:43 -0600

    Enabling Cache Busting

--- library/primer.php
+++ library/primer.php
@@ -68,10 +68,6 @@ function primer_head_cleanup() {
 	remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
 	// WP version
 	remove_action( 'wp_head', 'wp_generator' );
-  // remove WP version from css
-  add_filter( 'style_loader_src', 'primer_remove_wp_ver_css_js', 9999 );
-  // remove Wp version from scripts
-  add_filter( 'script_loader_src', 'primer_remove_wp_ver_css_js', 9999 );
 
 } /* end primer head cleanup */
 
